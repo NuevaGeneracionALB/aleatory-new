4a7053ecffd7872be1b158a89f2e1fd84acb9ec0
Aleatory Conteúdos <aleatory@gmail.com>
2023-05-03 01:18:21 -0300

    first commit

---
+++ dono/textos.js
@@ -0,0 +1,45 @@
+var TEXTOS_GERAL = {
+
+// MENSAGEM DE COMANDO NÃO ENCONTRADO: O #PREFIXO# é a definição que pega o prefixo.
+CMD_N_ENCONTRADO: `_- Comando não encontrado: Utilize #PREFIXO#menu para visualizar meus comandos.`,
+
+// MENSAGEM DE QUANDO TENTAREM UTILIZAR UM COMANDO EM GRUPO, QUE ESTÁ BLOQUEADO.
+COMANDO_BLOQUEADO: `Comando Bloqueado, proibido por algum administrador do grupo, de utilizar este comando.`,
+
+// QUANDO BLOQUEIA O USUARIO DE UTILIZAR COMANDOS NO GRUPO.
+BLOQUEADO_DE_U_CMD: `Você está bloqueado de utilizar meus comandos, fale com algum administrador pra saber o porque, não pertube para não levar ban do grupo.`,
+
+// TEXTO QUE VAI APARECER QUANDO REMOVER UM USUARIO QUE ESTÁ NA LISTA NEGRA.
+LISTA_NEGRA_T: `Este usuario se encontra na lista negra, então por ordem dos administradores do grupo, irei remover ele(a)`,
+
+// TEXTO QUE VAI APARECER QUANDO REMOVER UM USUARIO QUE ESTÁ NA LISTA NEGRA GLOBALMENTE
+LISTA_NEGRA_GLOBAL: `Este usuario se encontra na lista negra, então por ordem do meu dono, irei remover ele(a)`,
+
+// TEXTO QUE VAI APARECER, QUANDO REMOVER UM ESTRANGEIRO, LEMBRE-SE DE ADD O DDI, O DO BRASIL É 55:
+ANTI_ESTRANGEIRO_T: `Usuario estrangeiro não permitido entrou no grupo, irei remover ele por não está na lista de DDI permitido do país dele, Fale com algum administrador`,
+
+// MENSAGEM DE QUE REMOVEU O USARIO QUE ENVIOU LINK.
+REMOVEU_LINK: `Usuario enviou um link, e não é permitido no grupo, adeus.`,
+
+// REMOVER USUARIOS QUE EXCEDE O LIMITE DE CARACTERES, DO COMANDO LIMITECARACTERES OU USANDO O DIMINUITIVO LIMITEC, A SIGLA #LMTCRC# É UMA DEFINIÇÃO DO LIMITE QUE NÃO PODE EXCEDER, QUE VAI APARECER QUANDO ENVIAR.
+LIMITE_CARACTERES: `Usuario excedeu o limite de caracteres permitida pelos administradores, então irei remover, Limite permitido: #LMTCRC#`,
+
+// TODO COMANDO QUE QUEM NÃO FOR DONO, E FOR EXECUTADO POR ALGUM USUARIO, O BOT VAI RESPONDER:
+DONO: `Olá, apenas meu dono pode utilizar este comando.`,
+
+// TODO COMANDO QUE QUEM NÃO FOR ADM, E FOR EXECUTADO POR ALGUM USUARIO, O BOT VAI RESPONDER:
+ADM: `Olá, apenas administradores do grupo pode utilizar este comando.`,
+
+// TODO COMANDO QUE O BOT PRECISA SER ADM, O BOT VAI RESPONDER:
+BOT_ADM: `Olá, Eu preciso ser administrador para poder realizar esta ação.`,
+
+// QUANDO UM COMANDO DE GRUPO É EXECUTADO NO PRIVADO.
+SO_GRUPO: `Este comando so deve ser utilizado em grupos.`,
+
+// TODO COMANDO DE AÇÃO QUE FOR UTILIZADO, O BOT VAI RESPONDER:
+ENV: `Aguarde, estou realizando a ação..`
+
+};
+
+// NÃO MEXA NISSO, SÓ PODE MODIFICAR O TEXTO QUE ESTÁ ENTRE ` `, CUIDADO PARA NÃO APAGAR AS CHAVES, TIPO { }
+exports.TEXTOS_GERAL = TEXTOS_GERAL;
---
+++ readme.md
@@ -0,0 +1,19 @@
+Para utilizar em Ubuntu, instale o Chrome stable.
+
+Ubuntu 20.04:
+https://sempreupdate.com.br/como-instalar-o-google-chrome-no-ubuntu-20-04/
+
+Pra outras versões, pesquise no Google kkkk, já pra Windows, você ver como instalar ffmpeg, nesse vídeo: https://youtu.be/hHhyC0mL8AU
+
+E o nodejs, depois final de vez usar o github do aleatory-md
+
+Você usa do aleatory-new.
+
+git clone https://github.com/NuevaGeneracionALB/aleatory-md
+
+
+
+Já se não tiver onde hospedar, você pode pagar hospedagem, lá na bronxyshost.com, e lá tem o bot já pra instalar direto, quiser pagar a hospedagem via pix, chama lá: https://wa.me/558198923680
+
+
+Meu grupo / para suporte: https://chat.whatsapp.com/FT9Szs4fMom7nS2qFNMmye# aleatory-new
